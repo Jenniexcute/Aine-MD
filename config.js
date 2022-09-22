b0a1955f674bc8637ca9b6a742d8e20fb2698705
Jennie baby <108253179+lilybotz@users.noreply.github.com>
2022-09-21 16:03:33 -1200

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['94726083178', 'lakshan', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
